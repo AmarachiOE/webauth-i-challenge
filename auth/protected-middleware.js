af3c3641e43e6f0c5fb74a594b97c13a084a0593
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-07 11:46:15 -0700

    Updated protected-middleware to be based off of presence of session instead of headers.

--- auth/protected-middleware.js
+++ auth/protected-middleware.js
@@ -1,33 +1,12 @@
 // Middleware
+
 // don't need to import bcrypt and usersDb since using sessions
 
-const bcrypt = require("bcryptjs");
-const usersDb = require("../database/helpers/users-model.js");
-
 function protected(req, res, next) {
-
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    // from Login endpoint
-    usersDb.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res
-            .status(401)
-            .json({ message: "Invalid credentials. Access denied." });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "Something went wrong. Unable to validate your credentials."
-        });
-      });
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(401).json({ error: "Invalid credentials. Access denied." });
+    res.status(401).json({ message: "Please login to access resources" });
   }
 }
 
