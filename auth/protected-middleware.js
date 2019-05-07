ec0eb1cf8ca6f8a90bee354a39c451d556d7a052
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-07 11:56:54 -0700

    Logout functionality completed.

--- auth/protected-middleware.js
+++ auth/protected-middleware.js
@@ -6,7 +6,7 @@ function protected(req, res, next) {
   if (req.session && req.session.username) {
     next();
   } else {
-    res.status(401).json({ message: "Please login to access resources" });
+    res.status(401).json({ message: "Please login to access resources." });
   }
 }
 
